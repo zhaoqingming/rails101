0b8f2298883e26fcd9f290df4cb616afe0f950d4
zhaoqingming <3014zhaoqingming@gmail.com>
2017-06-01 20:00:22 +0800

    sdd post and hook group/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :groups       
+  has_many :groups
+  has_many :posts     
 end
