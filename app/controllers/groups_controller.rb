12d2b00a36fd3ffb873842ddea2d41a0f0f40db8
zhaoqingming <3014zhaoqingming@gmail.com>
2017-06-02 22:03:41 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :jion, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -31,11 +31,7 @@ class GroupsController < ApplicationController
 
 
   def update
-
-
-
   if @group.update(group_params)
-
     redirect_to groups_path, notice: "Update Success"
   else
        render :edit
@@ -43,14 +39,38 @@ class GroupsController < ApplicationController
  end
 
   def destroy
-
-
     @group.destroy
-
     redirect_to groups_path, alert: "Group deleted"
   end
 
- private
+def join
+  @group = Group.find(params[:id])
+
+   if !current_user.is_member_of?(@group)
+     current_user.join!(@group)
+     flash[:notice] = "加入本讨论版成功！"
+   else
+    flash[:warning] = "你已经是本讨论版成员了！"
+   end
+
+   redirect_to group_path(@group)
+end
+
+def quit
+  @group = Group.find(params[:id])
+
+  if cueeren_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert] = "已退出本讨论版！"
+  else
+    flash[:warning] = "你不是本讨论班成员，怎么退出 XD"
+  end
+
+  redirect_to group_path(@group)
+end
+
+
+private
 
  def find_group_and_check_permission
    @group = Group.find(params[:id])
